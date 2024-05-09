89599ff42c2ef704cd5df79e514862e7312a6eaf
Dany246 <145447252+Dany246@users.noreply.github.com>
2024-05-09 14:20:19 +0200

    fooldal

---
+++ fooldal.js
@@ -0,0 +1,58 @@
+function hehe(){
+    document.body.style.backgroundColor = "rgb(255, 255, 255)";
+    document.body.style.color = "rgb(0, 0, 0)";
+    document.getElementById('h').style.color = "rgb(255, 255, 255)";
+    document.getElementById('cim').style.backgroundColor = "rgb(0, 0, 0)";
+    document.getElementById('button').style.backgroundColor = "rgb(0, 0, 0)";
+    document.getElementById('button').style.color = "rgb(255, 255, 255)";
+    document.getElementById('button2').style.backgroundColor = "rgb(0, 0, 0)";
+    document.getElementById('button2').style.color = "rgb(255, 255, 255)";
+    document.getElementById('kep').style.borderColor = "rgb(0,0,0)";
+    document.getElementById('kep1').style.borderColor = "rgb(0,0,0)";
+    document.getElementById('kep2').style.borderColor = "rgb(0,0,0)";
+    document.getElementById('kep3').style.borderColor = "rgb(0,0,0)";
+    document.getElementById('szoveg').style.backgroundColor = "rgb(0,0,0)";
+    document.getElementById('szoveg').style.color = "rgb(255,255,255)";
+    document.getElementById('szoveg2').style.backgroundColor = "rgb(0,0,0)";
+    document.getElementById('szoveg2').style.color = "rgb(255,255,255)";
+    document.getElementById('szoveg3').style.backgroundColor = "rgb(0,0,0)";
+    document.getElementById('szoveg3').style.color = "rgb(255,255,255)";
+    document.getElementById('button3').style.backgroundColor = "rgb(255, 255, 255)";
+    document.getElementById('button3').style.color = "rgb(0,0,0)";
+    document.getElementById('button4').style.backgroundColor = "rgb(255, 255, 255)";
+    document.getElementById('button4').style.color = "rgb(0,0,0)";
+    document.getElementById('button5').style.backgroundColor = "rgb(255, 255, 255)";
+    document.getElementById('button5').style.color = "rgb(0,0,0)";
+    document.getElementById('szoveg4').style.backgroundColor = "rgb(0,0,0)";
+    document.getElementById('szoveg4').style.color = "rgb(255,255,255)";
+}
+
+function hihi(){
+    document.body.style.backgroundColor = "rgb(0, 0, 0)";
+    document.body.style.color = "rgb(255, 255, 255)";
+    document.getElementById('h').style.color = "rgb(0, 0, 0)";
+    document.getElementById('cim').style.backgroundColor = "rgb(255, 255, 255)";
+    document.getElementById('button').style.backgroundColor = "rgb(255, 255, 255)";
+    document.getElementById('button').style.color = "rgb(0, 0, 0)";
+    document.getElementById('button2').style.backgroundColor = "rgb(255, 255, 255)";
+    document.getElementById('button2').style.color = "rgb(0, 0, 0)";
+    document.getElementById('kep').style.borderColor = "rgb(255,255,255)";
+    document.getElementById('kep1').style.borderColor = "rgb(255,255,255)";
+    document.getElementById('kep2').style.borderColor = "rgb(255,255,255)";
+    document.getElementById('kep3').style.borderColor = "rgb(255,255,255)";
+    document.getElementById('szoveg').style.backgroundColor = "rgb(255,255,255)";
+    document.getElementById('szoveg').style.color = "rgb(0,0,0)";
+    document.getElementById('szoveg2').style.backgroundColor = "rgb(255,255,255)";
+    document.getElementById('szoveg2').style.color = "rgb(0,0,0)";
+    document.getElementById('button3').style.backgroundColor = "rgb(0, 0, 0)";
+    document.getElementById('button3').style.color = "rgb(255,255,255)";
+    document.getElementById('button4').style.backgroundColor = "rgb(0, 0, 0)";
+    document.getElementById('button4').style.color = "rgb(255,255,255)";
+    document.getElementById('button5').style.backgroundColor = "rgb(0, 0, 0)";
+    document.getElementById('button5').style.color = "rgb(255,255,255)";
+    document.getElementById('szoveg4').style.backgroundColor = "rgb(255,255,255)";
+    document.getElementById('szoveg4').style.color = "rgb(0,0,0)";
+    document.getElementById('szoveg3').style.backgroundColor = "rgb(255,255,255)";
+    document.getElementById('szoveg3').style.color = "rgb(0,0,0)";
+}
+
